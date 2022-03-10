621dd4135bc43e2f073f4d8042f2f5022d24a987
Mobulos <fabian.schmeltzer77@outlook.de>
2022-03-10 08:09:36 +0100

    init install

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Init
+
+https://bit.ly/3CuNOiu
+
+# root check
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
+if [ "$(id -u)" != "0" ]; then
+  echo "Das Script muss als root gestartet werden." 1>&2
+  exit 1
+fi
+
+apt-get install -y sudo || :
+sudo apt-get install -y curl
+clear
+curl --progress-bar https://raw.githubusercontent.com/Mobulos/template/master/start.sh --output start.sh
+read -t 1
+chmod +x start.sh
+clear
+./start.sh
