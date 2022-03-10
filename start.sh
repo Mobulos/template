6a9879a79b5d9bbd1c4415cb5135d23512930c87
Mobulos <fabian.schmeltzer77@outlook.de>
2022-03-10 07:54:43 +0100

    init start

---
+++ start.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+############################################
+################# CHANGE ###################
+ver=0.0.1
+dat=10.03.2022
+filescript=start.sh
+link=https://raw.githubusercontent.com/Mobulos/MultiscriptByMobulos/master/MultiscriptByMobulos.sh
+############################################
+############################################
+
+#JumpTo Funktion init
+function jumpto
+{
+    label=$1
+    cmd=$(sed -n "/$label:/{:a;n;p;ba};" $0 | grep -v ':$')
+    eval "$cmd"
+    exit
+}
+
+#JumpTo Definitionen
+menue=${1:-"menue"}
+install=${2:-"install"}
+
+# Farbcodes definieren
+red=($(tput setaf 1))
+green=($(tput setaf 2))
+yellow=($(tput setaf 3))
+reset=($(tput sgr0))
+
+#Farbcode beim start reseten
+echo "$reset"
+
+#Root Check
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
+if [ "$(id -u)" != "0" ]; then
+    echo "Das Script muss als root gestartet werden." 1>&2
+    exit 1
+fi
+
+#Internet check
+if ping -c 1 raw.githubusercontent.com; then
+    echo ""
+else
+    clear
+    echo "Es konnte keine Netzwerk Verbindung zu den GitHub Servern hergestellt werden."
+    echo "Das Script kann ohne diese Verbindung nicht ausgeführt werden."
+    echo "Bitte überprüfe deine Internet Verbingung."
+    exit 1
+fi
+
+install:
+    apt-get update ||:
+    apt-get install -y sudo
+    apt-get update
+    for i in wget
+    do
+        apt-get install -y $i
+    done
+
+menue:
+    echo "$yellow########################################"
+    read -t0.1
+    echo "#####  Temp Script by Mobulos  #####"
+    read -t0.1
+    echo "########################################"
+    read -t0.1
+    echo
+    echo "$reset"
+    read -t0.1
+    echo "Version: $ver"
+    read -t0.1
+    echo "Update vom: $dat"
+
+    tmp=($(tput setaf 2)) && echo "$tmp"
+    read -t0.1
+    echo "  1. Bot installieren"
